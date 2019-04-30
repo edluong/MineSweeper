6397a0f0958f7a12b29881f4dc1d496ebb9a2c2d
Ed <edmundluong@gmail.com>
2019-04-29 20:38:01 -0700

    created square class

--- src/board.py
+++ src/board.py
@@ -1,3 +1,5 @@
+from square import *
+
 class Board:
     '''
         Features:
@@ -9,24 +11,21 @@ class Board:
     '''
 
     def __init__(self,row,col):
-        self.row = row
-        self.col = col
-        self.board = [[0] * row for i in range(col)] # initialize empty board
+        self.row, self.col = row, col
+        self.board = [[Square(r,c) for r in range(self.row)] for c in range(self.col)] # initialize empty board
 
-
+    
     def print_board(self):
-            '''
-                method that will print out the elements inside the board
-            '''
-            # created a nicer print view for the user to see
-            for row in range(self.row):
-                for col in range(self.col):
-                    # if at the end of the row
-                    if col == self.col - 1:
-                        # create an empty row then start on the next one
-                        print(self.board[col][row],end='\n\n')
-                    else:
-                        # print out each row element with a tab
-                        print(self.board[col][row],end='\t')
-                   
-                    
+        '''
+            method that will print out the elements inside the board
+        '''
+        # created a nicer print view for the user to see
+        for r in range(self.row):
+            for c in range(self.col):
+                # if at the end of the row
+                if c == self.col - 1:
+                    # create an empty row then start on the next one
+                    print(self.board[r][c].coordinates,end='\n\n')
+                else:
+                    # print out each row element with a tab
+                    print(self.board[r][c].coordinates,end='\t')                   
---
+++ src/square.py
@@ -0,0 +1,15 @@
+class Square:
+    def __init__(self, row, col, mines = 0, hasFlag = False, isVisible = False ):
+        self.coordinates = (row,col)
+        self.mines = mines
+        self.hasFlag = hasFlag
+        self.isVisible = isVisible
+
+    def place_flag(self):
+        self.hasFlag = True
+
+    def show_visible(self):
+        self.isVisible = True
+    
+    def __str__(self):
+        return self.coordinates
